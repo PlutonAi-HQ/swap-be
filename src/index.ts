b66faf6bf97feb2b51392a41d8214eed1c44b83e
phuquiivo03 <52100996@student.tdtu.edu.vn>
2025-01-22 14:14:17 +0700

    :rocket: feat: update place order

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import cors from 'cors';
-import express from 'express';
-import dotenv from 'dotenv';
-import { PublicKey } from '@solana/web3.js';
-import { init } from './util/init.js';
-import { jupiterLimitOrder, jupiterTrade } from './util/jupiter.js';
-import type {ILimitOrder, ISwapReqest} from './type.js';
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+import { PublicKey } from "@solana/web3.js";
+import { init } from "./util/init.js";
+import { jupiterLimitOrder, jupiterTrade } from "./util/jupiter.js";
+import type { ILimitOrder, ISwapReqest } from "./type.js";
 
 //init keypair
 
@@ -17,60 +17,59 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: '10mb' }));
-app.get('/', async (_req, res) => {
+app.use(express.json({ limit: "10mb" }));
+app.get("/", async (_req, res) => {
   res.send(`Expressjs is running!`);
 });
 
-
-app.use('/jupiterLimitOrder', async(_req, res) => {
+app.use("/jupiterLimitOrder", async (_req, res) => {
   try {
+    console.log(_req.body);
     // convert body to ILimitOrder
-    const body: ILimitOrder  = _req.body; 
-    const {wallet} = init(body.privateKey);
+    const body: ILimitOrder = _req.body;
+    const { wallet } = init(body.privateKey);
 
     const data = await jupiterLimitOrder(
-      body.inputMint,
-      body.outputMint,
-      body.inputMint, //usdc
-      body.outputMint, //usdc
+      body.makingAmount, // amount of token to sell
+      body.takingAmount, // amount of token to buy
+      new PublicKey(body.inputMint), // input token address
+      new PublicKey(body.outputMint), // output token address
       wallet
-    )
+    );
     res.send(data);
   } catch (err) {
+    console.log(err);
     res.send("Invalid request body");
   }
-})
+});
 
-app.get('/jupiterSwap', async (_req, res) => {
+app.get("/jupiterSwap", async (_req, res) => {
   try {
     // convert body to ISwapReqest
     const body: ISwapReqest = _req.body;
     // init wallet and keypair
-    const {wallet, keypair} = init(body.privateKey);
+    const { wallet, keypair } = init(body.privateKey);
     console.log("jupiter swapp");
-    
+
     try {
       const result = await jupiterTrade(
         new PublicKey(body.inputMint), // input token address
         body.inputAmount, // amount of input token
         new PublicKey(body.outputMint), // output token address
         100, // 1% slippage
-        20000000, // 20000000 lamports priority fee
+        100000000, // 20000000 lamports priority fee
         wallet, // user waller
         keypair
       );
 
-      res.send(result);  
-    }catch(err) {
-      throw(err);
+      res.send(result);
+    } catch (err) {
+      throw err;
     }
-  } catch(err) {
+  } catch (err) {
     res.send("Invalid request body");
   }
-
 });
-
 
 app.listen(3000, () => {
   console.log(`REST API is listening on port: ${3000}.`);
--- src/util/jupiter.ts
+++ src/util/jupiter.ts
@@ -1,51 +1,49 @@
-import { PublicKey } from '@solana/web3.js';
+import { PublicKey } from "@solana/web3.js";
 import { getMint } from "@solana/spl-token";
 import { VersionedTransaction, SendOptions } from "@solana/web3.js";
-import { Wallet } from '@project-serum/anchor';
-import {connection} from './init.js';
-import { Keypair } from '@solana/web3.js';
-import fetch from 'node-fetch';
+import { Wallet } from "@project-serum/anchor";
+import { connection } from "./init.js";
+import { Keypair } from "@solana/web3.js";
+import fetch from "node-fetch";
 
 type CreateOrder = {
-    inputMint: string;
-    outputMint: string;
-    maker: string;
-    payer: string;
-    params: {
-      makingAmount: string;
-      takingAmount: string;
-      expiredAt?: string;
-      feeBps?: string;
-    };
-    computeUnitPrice: string | "auto";
-    referral?: string;
-    inputTokenProgram?: string;
-    outputTokenProgram?: string;
-    wrapAndUnwrapSol?: boolean;
+  inputMint: string;
+  outputMint: string;
+  maker: string;
+  payer: string;
+  params: {
+    makingAmount: string;
+    takingAmount: string;
+    expiredAt?: string;
+    feeBps?: string;
   };
-  
-  type CreateOrderResponse = {
-    order: string;
-    tx: string;
-  };
+  computeUnitPrice: string | "auto";
+  referral?: string;
+  inputTokenProgram?: string;
+  outputTokenProgram?: string;
+  wrapAndUnwrapSol?: boolean;
+};
+
+type CreateOrderResponse = {
+  order: string;
+  tx: string;
+};
 
 interface Response {
-    status: boolean;
-    data: string;
+  status: boolean;
+  data: string;
 }
 
 export async function jupiterTrade(
-    inputMint: PublicKey, // the input token to swap from
-    inputAmount: number, // the amount of input token to swap
-    outputMint: PublicKey, // the output token to swap to
-    slippageBps: number, // slipage
-    priorityFee: number, // priority fee
-    wallet: Wallet, // user wallet
-    keypair: Keypair // user public key
-  
+  inputMint: PublicKey, // the input token to swap from
+  inputAmount: number, // the amount of input token to swap
+  outputMint: PublicKey, // the output token to swap to
+  slippageBps: number, // slipage
+  priorityFee: number, // priority fee
+  wallet: Wallet, // user wallet
+  keypair: Keypair // user public key
 ): Promise<Response> {
   try {
-
     // Convert input amount to correct decimals
     const inputDecimals = (await getMint(connection, inputMint)).decimals;
 
@@ -66,7 +64,7 @@ export async function jupiterTrade(
     ).json();
 
     // Get serialized transaction
-    
+
     // create a transaction
     const { swapTransaction } = await (
       await fetch("https://quote-api.jup.ag/v6/swap", {
@@ -83,101 +81,100 @@ export async function jupiterTrade(
           feeAccount: null,
           priorityLevelWithMaxLamports: {
             maxLamports: priorityFee, // max priority fee
-            priorityLevel: "veryHigh" // If you want to land transaction fast, set this to use `veryHigh`. You will pay on average higher priority fee.
-          }
+            priorityLevel: "veryHigh", // If you want to land transaction fast, set this to use `veryHigh`. You will pay on average higher priority fee.
+          },
         }),
       })
     ).json();
     // Deserialize transaction
     const swapTransactionBuf = Buffer.from(swapTransaction, "base64");
-  // Deserialize transaction
+    // Deserialize transaction
     const transaction = VersionedTransaction.deserialize(swapTransactionBuf);
     // Sign and send transaction
     transaction.sign([wallet.payer]);
     const latestBlockHash = await connection.getLatestBlockhash();
-    const rawTransaction = transaction.serialize()
+    const rawTransaction = transaction.serialize();
     const txid = await connection.sendRawTransaction(rawTransaction, {
       skipPreflight: true,
-      maxRetries: 2
+      maxRetries: 2,
     });
-    
+
     // Wait for transaction to be confirmed (submit to blockchain)
     await connection.confirmTransaction({
       blockhash: latestBlockHash.blockhash,
       lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-      signature: txid
-     });
+      signature: txid,
+    });
     // const signature = await connection.sendTransaction(transaction);
-    return {status: true, data: txid};
+    return { status: true, data: txid };
   } catch (error: any) {
-    return {status: false, data: error.message};
+    return { status: false, data: error.message };
   }
-
-
-  
-  
 }
 
 // Get the serialized transactions to create the limit order
 export async function jupiterLimitOrder(
-    makingAmount: string, // amount of token to sell
-    takingAmount: string, // amount of token to buy
-    inputMint: string, // input token mint address
-    outputMint: string, // output token mint address
-    wallet: Wallet // user wallet
-    
+  makingAmount: number, // amount of token to sell
+  takingAmount: number, // amount of token to buy
+  inputMint: PublicKey, // input token mint address
+  outputMint: PublicKey, // output token mint address
+  wallet: Wallet // user wallet
 ) {
-    const createOrderBody: CreateOrder = {
-        maker: wallet.publicKey.toBase58(),
-        payer: wallet.publicKey.toBase58(),
-        inputMint,
-        outputMint,
-        params: {
-          makingAmount, // 0.05 SOL
-          takingAmount, // 100 USDC
-        },
-      
-        // "auto" sets the priority fee based on network congestion
-        // and it will be capped at 500,000
-        computeUnitPrice: "auto",
-      };
+  const inputDecimals = (await getMint(connection, inputMint)).decimals;
+  const outputDecimals = (await getMint(connection, outputMint)).decimals;
 
-      //Send request to API server
+  // Calculate the correct amount based on actual decimals
+  const inputScaleAmount = makingAmount * Math.pow(10, inputDecimals);
+  const outputScaleAmount = takingAmount * Math.pow(10, outputDecimals);
+  const createOrderBody: CreateOrder = {
+    inputMint: inputMint.toString(),
+    outputMint: outputMint.toString(),
+    maker: wallet.publicKey.toBase58(),
+    payer: wallet.publicKey.toBase58(),
+    params: {
+      makingAmount: `${inputScaleAmount}`,
+      takingAmount: `${outputScaleAmount}`,
+    },
 
-      const fetchOpts = {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(createOrderBody),
-      };
-      
-      const response = await fetch(
-        "https://api.jup.ag/limit/v2/createOrder",
-        fetchOpts
-      );
+    // "auto" sets the priority fee based on network congestion
+    // and it will be capped at 500,000
+    computeUnitPrice: "auto",
+  };
+  console.log("body", createOrderBody);
 
-    //   Sign and submit the transaction on chain
+  //Send request to API server
 
-    // Deserialise base64 tx response
-    const { order, tx } = await response.json();
-    const txBuff = Buffer.from(tx, "base64");
-    const vtx = VersionedTransaction.deserialize(txBuff);
+  const fetchOpts = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(createOrderBody),
+  };
 
-    // Sign with wallet
-    try {
-        vtx.sign([wallet.payer]);
+  const response = await fetch(
+    "https://api.jup.ag/limit/v2/createOrder",
+    fetchOpts
+  );
+
+  //   Sign and submit the transaction on chain
+
+  // Deserialise base64 tx response
+  const { order, tx } = await response.json();
+  const txBuff = Buffer.from(tx, "base64");
+  const vtx = VersionedTransaction.deserialize(txBuff);
+
+  // Sign with wallet
+  try {
+    vtx.sign([wallet.payer]);
     const rpcSendOpts: SendOptions = { skipPreflight: true };
     const hash = await connection.sendRawTransaction(
-    vtx.serialize(),
-    rpcSendOpts
+      vtx.serialize(),
+      rpcSendOpts
     );
-    return {data: hash, status: true};
-    } catch(e: any) {
-        return  {data: e.message, status: false};
-    }
+    return { data: hash, status: true };
+  } catch (e: any) {
+    return { data: e.message, status: false };
+  }
 }
-
-
-
